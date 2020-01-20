acc1f889817cd07fa6171e193ba5bdf1dfdf39c8
Gregory Losco <gtlosco@gmail.com>
2020-01-20 22:09:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,8 @@ module FormsLab
     post '/pirates' do 
       @pirate = Pirate.new(params[:pirate])
       
-      params[:pirate][:ships].each do |ship|
+      params[:pirate][:ships].each do |t|
+        
       end
       
       @ship = Ship.new()
