317a42f56d71cd7b17f80f10608f44f85669c8e4
Gregory Losco <gtlosco@gmail.com>
2020-01-20 22:08:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,5 +5,21 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do 
+      erb :root 
+    end
+    
+    get '/new' do 
+      erb :'pirates/new' 
+    end
+    
+    post '/pirates' do 
+      @pirate = Pirate.new(params[:pirate])
+      
+      @ship = Ship.new(params[:pirate][:ships])
+      
+      erb :'pirates/show'
+    end
+
   end
 end
